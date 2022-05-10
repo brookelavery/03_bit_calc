f33ab14528b5b702f59183d5d3cd76819ed218a6
brookelavery <101751141+brookelavery@users.noreply.github.com>
2022-05-10 12:07:53 +1200

    Add files via upload

---
+++ 01_statement_generator.py
@@ -0,0 +1,19 @@
+
+
+
+def statement_generator(text, decoration):
+
+
+    ends = decoration * 5
+
+
+    statement = "{} {} {}".format(ends, text, ends)
+
+    print()
+    print(statement)
+    print()
+
+    return ""
+
+
+statement_generator("hello world", "-")
